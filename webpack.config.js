0a0d6694400cf49b637a345f60eee848f05a2f62
butterflyguo <422859608@qq.com>
2020-07-01 16:40:48 +0800

    webpack配置补充

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: "production", //模式 不写默认是production,   production:打包后会进行压缩，development不会进行压缩
     entery: "./index.js", // 入口文件
     output: { //出口
         filename: 'mian.js', //文件名

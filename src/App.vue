9db0851ea60747be91c1a8f43ea38a77e77f93d0
butterflyguo <422859608@qq.com>
2020-07-02 15:13:04 +0800

    file-loader及url-loader的基本使用

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <img :src="ava" alt="">
+</div>
+</template>
+<script>
+import ava from './assets/imgs/icon.jpg'
+export default {
+    data(){
+        return {
+            ava
+        }
+    }
+}
+</script>
+<style >
+
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,46 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //通过自动注入所有生成的包来为应用程序生成一个HTML文件
+const VueLoaderPlugin = require('vue-loader/lib/plugin'); //  它的职责是将你定义过的其它规则复制并应用到 .vue 文件里相应语言的块。例如，如果你有一条匹配 /\.js$/ 的规则，那么它会应用到 .vue 文件里的 <script> 块。
 
 module.exports = {
-    mode: "production", //模式 不写默认是production,   production:打包后会进行压缩，development不会进行压缩
-    entery: "./index.js", // 入口文件
+    mode: "development", //模式 不写默认是production,   production:打包后会进行压缩，development不会进行压缩
+    entry: "./src/main.js", // 打包入口文件
+    module: {
+        rules:[
+        //     {
+        //     test: /\.(png|jpe?g|gif)$/i, //不需要加引号
+        //     use: [{
+        //             loader: 'file-loader', //将文件发送到输出文件夹并返回(相对)URL
+        //             options: {
+        //                 name: '[name]_[hash].[ext]', //使打包后的文件名为： 原本文件名_hash值.文件类型
+        //                 outputpath: 'imgs/' 
+        //             }
+        //     }]
+        // },
+        {
+            test: /\.(png|jpe?g|gif)$/i, //不需要加引号
+            use: [{
+                    loader: 'url-loader', //将文件发送到输出文件夹并返回(相对)URL
+                    options: {
+                        name: '[name]_[hash].[ext]', //使打包后的文件名为： 原本文件名_hash值.文件类型
+                        outputpath: 'imgs/' ,
+                        limit: 20480 //20kb 大于20kb的图片会像file-loader一样打包到dist/imgs下，如果小于20kb会将图片压缩成base64位,并添加到dist目录下输出js文件里
+                    }
+            }]
+        },
+        {
+            test: /\.vue$/,
+            loader: 'vue-loader' //用来打包.vue文件模块
+           
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './public/index.html'}),
+        // 请确保引入这个插件！
+        new VueLoaderPlugin() 
+      ],
     output: { //出口
-        filename: 'mian.js', //文件名
-        path: path.resolve(_dirname,'dist') //出口文件路径 绝对地址
+        filename: 'app.js', //文件名
+        path: path.resolve(__dirname,'dist') //出口文件路径 绝对地址
     }
 }
